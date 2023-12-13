e82244c4773137a449f9518b5e834d96d727d0fd
aamir <aamir.khan2478@gmail.com>
2023-12-13 10:19:52 +0500

    Add configure fonts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,10 @@
-import { Inter } from "next/font/google";
+import { fonts } from "./fonts";
 import "./globals.css";
 import ChakraUIProvider from "@/components/providers/ChakraUIProvider";
 import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
 import { ColorModeScript } from "@chakra-ui/react";
 import theme from "@/constants/theme";
 import "react-phone-input-2/lib/style.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "BMS",
@@ -15,8 +13,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className={fonts.rubik.variable}>
+      <body>
         <ReactQueryProvider>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <ChakraUIProvider>{children}</ChakraUIProvider>
