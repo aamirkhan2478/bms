d369536c0dfffcc62b72684603dc30cf26cd0a19
aamir <aamir.khan2478@gmail.com>
2023-12-13 10:03:34 +0500

    Modified some routes

--- src/components/Sidebar/SidebarContent.jsx
+++ src/components/Sidebar/SidebarContent.jsx
@@ -1,8 +1,17 @@
 import { Box, CloseButton, Flex, useColorModeValue } from "@chakra-ui/react";
 import NavItem from "./NavItem";
 import { IoMdPersonAdd } from "react-icons/io";
-import { BsBuildingFillAdd, BsPersonFillAdd, BsSpeedometer } from "react-icons/bs";
-import { MdAssignment, MdDashboard, MdOutlineElectricMeter, MdSell } from "react-icons/md";
+import {
+  BsBuildingFillAdd,
+  BsPersonFillAdd,
+  BsSpeedometer,
+} from "react-icons/bs";
+import {
+  MdAssignment,
+  MdDashboard,
+  MdOutlineElectricMeter,
+  MdSell,
+} from "react-icons/md";
 import { FaMeteor } from "react-icons/fa";
 import Image from "next/image";
 import logo from "@/../public/bms_logo.png";
@@ -10,26 +19,38 @@ import logo from "@/../public/bms_logo.png";
 const SidebarContent = ({ onClose, ...rest }) => {
   const LinkItems = [
     { name: "Dashboard", icon: MdDashboard, path: "/dashboard" },
-    { name: "Add Inventory", icon: BsBuildingFillAdd, path: "/add-inventory" },
+    {
+      name: "Add Inventory",
+      icon: BsBuildingFillAdd,
+      path: "/dashboard/add-inventory",
+    },
     {
       name: "Add Wapda Submeter",
       icon: BsSpeedometer,
-      path: "/add-wapda-submeter",
+      path: "/dashboard/add-wapda-submeter",
     },
     {
       name: "Add Generator Submeter",
       icon: MdOutlineElectricMeter,
-      path: "/add-generator-submeter",
+      path: "/dashboard/add-generator-submeter",
     },
     {
       name: "Add Water Submeter",
       icon: FaMeteor,
-      path: "/add-water-submeter",
+      path: "/dashboard/add-water-submeter",
     },
-    { name: "Add Owner", icon: IoMdPersonAdd, path: "/add-owner" },
-    { name: "Add Tenant", icon: BsPersonFillAdd, path: "/add-tenant" },
-    { name: "Sell Inventory", icon: MdSell, path: "/sell-inventory" },
-    { name: "Add Contract", icon: MdAssignment, path: "/add-contract" },
+    { name: "Add Owner", icon: IoMdPersonAdd, path: "/dashboard/add-owner" },
+    {
+      name: "Add Tenant",
+      icon: BsPersonFillAdd,
+      path: "/dashboard/add-tenant",
+    },
+    { name: "Sell Inventory", icon: MdSell, path: "/dashboard/sell-inventory" },
+    {
+      name: "Add Contract",
+      icon: MdAssignment,
+      path: "/dashboard/add-contract",
+    },
   ];
   return (
     <Box
@@ -50,6 +71,7 @@ const SidebarContent = ({ onClose, ...rest }) => {
         <Box
           as={Image}
           src={logo}
+          alt="logo"
           h={150}
           w={150}
           filter={useColorModeValue("", "saturate(10)")}
