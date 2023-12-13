fb4c4456db98f75a8c5be3c85fa0604874ca88fd
aamir <aamir.khan2478@gmail.com>
2023-12-13 10:17:46 +0500

    Add new font

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -3,6 +3,10 @@ import { extendTheme } from "@chakra-ui/react";
 const config = {
   initialColorMode: "light",
   useSystemColorMode: false,
+  fonts: {
+    heading: 'var(--font-rubik)',
+    body: 'var(--font-rubik)',
+  }
 };
 
 const theme = extendTheme({ config });
