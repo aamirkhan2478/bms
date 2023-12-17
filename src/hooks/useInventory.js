8d1847f9efb0ecc5b510860d6d2a17204fafa7db
aamir <aamir.khan2478@gmail.com>
2023-12-17 23:26:47 +0500

    Add hook for hit an API endpoints

--- src/hooks/useInventory.js
+++ src/hooks/useInventory.js
@@ -1,12 +1,31 @@
 import axios from "@/utils/axiosInstance";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 const inventory = (values) => {
   return axios.post("/inventory/add", values);
 };
 
+const showInventories = () => {
+  return axios.get("/inventory/all");
+};
+
+const sellInventory = (values) => {
+  return axios.put("/inventory/sell", values);
+};
+
 export const useAddInventory = (onSuccess, onError) => {
   return useMutation(inventory, {
+    onSuccess,
+    onError,
+  });
+};
+
+export const useShowInventories = () => {
+  return useQuery("show-inventories", showInventories);
+};
+
+export const useSellInventory = (onSuccess, onError) => {
+  return useMutation(sellInventory, {
     onSuccess,
     onError,
   });
