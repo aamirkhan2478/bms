cafeaedb36682aaf5dfec2fbbee3bf8ae40e07a1
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-25 00:06:57 +0500

    Add Stepper component

---
+++ src/components/Stepper/index.jsx
@@ -0,0 +1,39 @@
+import {
+  Box,
+  Stepper as ChakraStepper,
+  Step,
+  StepDescription,
+  StepIcon,
+  StepIndicator,
+  StepNumber,
+  StepSeparator,
+  StepStatus,
+  StepTitle,
+} from "@chakra-ui/react";
+
+const Stepper = ({ steps, activeStep }) => {
+  return (
+    <ChakraStepper index={activeStep} display={{ base: "none", md: "flex" }}>
+      {steps.map((step, index) => (
+        <Step key={index}>
+          <StepIndicator>
+            <StepStatus
+              complete={<StepIcon />}
+              incomplete={<StepNumber />}
+              active={<StepNumber />}
+            />
+          </StepIndicator>
+
+          <Box flexShrink="0">
+            <StepTitle>{step.title}</StepTitle>
+            <StepDescription>{step.description}</StepDescription>
+          </Box>
+
+          <StepSeparator />
+        </Step>
+      ))}
+    </ChakraStepper>
+  );
+};
+
+export default Stepper;
