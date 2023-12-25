921c032b75a792dc896f4e3244b4a2046f33e864
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-25 20:49:05 +0500

    Update CustomButton component styling

--- src/components/CustomButton/index.jsx
+++ src/components/CustomButton/index.jsx
@@ -3,21 +3,13 @@ import { Button } from "@chakra-ui/react";
 const CustomButton = ({ text, ...rest }) => {
   return (
     <Button
-      fontSize="10px"
+      colorScheme="teal"
       type="submit"
-      bg="teal.300"
+      fontSize="10px"
       w="100%"
       h="45px"
       mb="20px"
-      color="white"
-      variant={"outline"}
       mt="20px"
-      _hover={{
-        bg: "teal.200",
-      }}
-      _active={{
-        bg: "teal.400",
-      }}
       {...rest}
     >
       {text}
