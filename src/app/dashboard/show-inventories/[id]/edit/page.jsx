1f74f031fd2852129ba925b67f66203debb678ea
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-28 00:25:51 +0500

    Add EditInventory component

---
+++ src/app/dashboard/show-inventories/[id]/edit/page.jsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+const EditInventory = () => {
+  return (
+    <Layout>
+      <div>EditInventory</div>
+    </Layout>
+  );
+};
+
+export default EditInventory;
