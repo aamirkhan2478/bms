368a4020ead00ae7294f2a8f9bdb714463e8fa12
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-28 00:25:45 +0500

    Add inventory details and owner information to ShowInventory component

--- src/app/dashboard/show-inventories/[id]/page.jsx
+++ src/app/dashboard/show-inventories/[id]/page.jsx
@@ -2,20 +2,63 @@
 import CustomBox from "@/components/CustomBox";
 import Layout from "@/components/Layout";
 import { useShowInventory } from "@/hooks/useInventory";
+import capitalizeFirstLetter from "@/utils/capitalizeFirstLetter";
+import dateFormat from "@/utils/dateFormat";
+import { Box, Divider, Flex, Heading, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "next/navigation";
 import React from "react";
 
 const ShowInventory = () => {
   const { id } = useParams();
   const { data, isLoading } = useShowInventory(id);
+  const inventory = data?.data?.data?.inventory;
 
   return (
     <Layout>
-      <CustomBox
-        heading={`${data?.data?.data.inventory?.inventoryType} - ${data?.data?.data.inventory?.floor}${data?.data?.data.inventory?.flatNo}`}
-        maxW={800}
-      >
-        <h1>Show Inventory</h1>
+      <CustomBox heading={"Inventory Details"} maxW={800}>
+        <Divider mb={6} />
+        {isLoading ? (
+          <Flex justifyContent={"center"} alignItems={"center"} h={"50vh"}>
+            <Spinner size={"xl"} color="teal" />
+          </Flex>
+        ) : (
+          <>
+            <Text mb={4}>
+              <strong>Inventory Type:</strong> {inventory?.inventoryType}
+            </Text>
+            <Text mb={4}>
+              <strong>Floor:</strong> {inventory?.floor}
+            </Text>
+            <Text mb={4}>
+              <strong>Flat No:</strong> {inventory?.flatNo}
+            </Text>
+
+            <Divider my={6} />
+            {inventory?.status === "sold" && (
+              <>
+                <Heading size="md" mb={4}>
+                  Owners Details
+                </Heading>
+
+                {inventory?.sellInventory.map((sell, index) => (
+                  <>
+                    {sell.isActive && (
+                      <Box key={index} mb={4}>
+                        <Text>
+                          <strong>Sold Date:</strong>{" "}
+                          {dateFormat(sell.purchaseDate)}
+                        </Text>
+                        <Text>
+                          <strong>Owner Name:</strong> {sell.ownerName}
+                        </Text>
+                      </Box>
+                    )}
+                  </>
+                ))}
+              </>
+            )}
+          </>
+        )}
       </CustomBox>
     </Layout>
   );
