978c6399bdbcff58e92aa6ac89a6778bc0a51b37
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-28 12:02:39 +0500

    Add inventory image and owner details to ShowInventory page

--- src/app/dashboard/show-inventories/[id]/page.jsx
+++ src/app/dashboard/show-inventories/[id]/page.jsx
@@ -2,11 +2,18 @@
 import CustomBox from "@/components/CustomBox";
 import Layout from "@/components/Layout";
 import { useShowInventory } from "@/hooks/useInventory";
-import capitalizeFirstLetter from "@/utils/capitalizeFirstLetter";
 import dateFormat from "@/utils/dateFormat";
 import { Box, Divider, Flex, Heading, Spinner, Text } from "@chakra-ui/react";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 import React from "react";
+import inventoryImage from "@/../public/inventory.png";
+import {
+  BiCalendar,
+  BiSolidBuildingHouse,
+  BiSolidUser,
+  BiUser,
+} from "react-icons/bi";
 
 const ShowInventory = () => {
   const { id } = useParams();
@@ -15,7 +22,17 @@ const ShowInventory = () => {
 
   return (
     <Layout>
-      <CustomBox heading={"Inventory Details"} maxW={800}>
+      <CustomBox
+        icon={
+          <Box
+            as={BiSolidBuildingHouse}
+            display={{ base: "none", sm: "flex" }}
+            color={"teal.300"}
+          />
+        }
+        heading={"Inventory Details"}
+        maxW={800}
+      >
         <Divider mb={6} />
         {isLoading ? (
           <Flex justifyContent={"center"} alignItems={"center"} h={"50vh"}>
@@ -23,39 +40,86 @@ const ShowInventory = () => {
           </Flex>
         ) : (
           <>
-            <Text mb={4}>
-              <strong>Inventory Type:</strong> {inventory?.inventoryType}
-            </Text>
-            <Text mb={4}>
-              <strong>Floor:</strong> {inventory?.floor}
-            </Text>
-            <Text mb={4}>
-              <strong>Flat No:</strong> {inventory?.flatNo}
-            </Text>
-
+            <Flex
+              direction={{ base: "column", sm: "row" }}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Box mb={4}>
+                <Image
+                  src={inventoryImage}
+                  width={300}
+                  height={400}
+                  alt="inventory"
+                />
+              </Box>
+              <Box>
+                <Text mb={4} fontSize={24}>
+                  <strong>Inventory Type:</strong> {inventory?.inventoryType}
+                </Text>
+                <Text mb={4} fontSize={24}>
+                  <strong>Floor:</strong> {inventory?.floor}
+                </Text>
+                <Text mb={4} fontSize={24}>
+                  <strong>Flat No:</strong> {inventory?.flatNo}
+                </Text>
+              </Box>
+            </Flex>
             <Divider my={6} />
-            {inventory?.status === "sold" && (
+            <Heading
+              w="100%"
+              textAlign={"center"}
+              fontWeight="normal"
+              mb="2%"
+              display={"flex"}
+              justifyContent={"center"}
+            >
+              <Box
+                as={BiSolidUser}
+                color={"teal.300"}
+                display={{ base: "none", sm: "flex" }}
+              />
+              Owners Details
+            </Heading>
+            <Divider mb={6} />
+            {inventory?.status === "sold" ? (
               <>
-                <Heading size="md" mb={4}>
-                  Owners Details
-                </Heading>
-
                 {inventory?.sellInventory.map((sell, index) => (
                   <>
                     {sell.isActive && (
-                      <Box key={index} mb={4}>
-                        <Text>
-                          <strong>Sold Date:</strong>{" "}
-                          {dateFormat(sell.purchaseDate)}
-                        </Text>
-                        <Text>
-                          <strong>Owner Name:</strong> {sell.ownerName}
-                        </Text>
+                      <Box
+                        key={index}
+                        mb={4}
+                        display={"flex"}
+                        flexDirection={"column"}
+                      >
+                        <Box display={"flex"} gap={2} alignItems={"center"}>
+                          <Box
+                            as={BiCalendar}
+                            fontSize={24}
+                            color={"blue.300"}
+                          />
+                          <Text fontSize={24}>
+                            <strong>Sold Date:</strong>{" "}
+                            {dateFormat(sell.purchaseDate)}
+                          </Text>
+                        </Box>
+                        <Box display={"flex"} gap={2} alignItems={"center"}>
+                          <Box as={BiUser} fontSize={24} color={"blue.300"} />
+                          <Text fontSize={24}>
+                            <strong>Owner Name:</strong> {sell.ownerName}
+                          </Text>
+                        </Box>
+                        <Divider my={6} />
                       </Box>
                     )}
                   </>
                 ))}
               </>
+            ) : (
+              <Text textAlign={"center"} color={"orange.500"} fontSize={20}>
+                No Owners Found, This Inventory is for sell.
+              </Text>
             )}
           </>
         )}
