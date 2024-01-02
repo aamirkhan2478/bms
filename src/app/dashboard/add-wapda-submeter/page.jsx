f9ed77639c6cce5dc79fa7071b5b496bbce607a1
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-01-02 14:30:33 +0500

    Add Breadcrumb component and update Breadcrumb usage

--- src/app/dashboard/add-wapda-submeter/page.jsx
+++ src/app/dashboard/add-wapda-submeter/page.jsx
@@ -1,9 +1,10 @@
 "use client";
+import Breadcrumb from "@/components/Breadcrumb";
 import CustomBox from "@/components/CustomBox";
 import CustomButton from "@/components/CustomButton";
 import Layout from "@/components/Layout";
 import TextField from "@/components/TextField";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, FormControl, FormHelperText, Heading, useColorModeValue } from "@chakra-ui/react";
+import { FormControl, FormHelperText } from "@chakra-ui/react";
 import { Formik } from "formik";
 import { useEffect } from "react";
 
@@ -32,28 +33,14 @@ const WapdaSubmeter = () => {
     document.title = "Add Wapda Submeter";
   }, []);
 
-  let mainText = useColorModeValue("gray.700", "gray.200");
-  let secondaryText = useColorModeValue("gray.400", "gray.400");
   return (
     <Layout>
-      <Heading>Add Wapda Submeter</Heading>
-      <Breadcrumb>
-        <BreadcrumbItem color={mainText}>
-          <BreadcrumbLink
-            href="#"
-            color={secondaryText}
-            textDecoration={"none"}
-          >
-            Add Wapda Submeter
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-
-        <BreadcrumbItem color={mainText}>
-          <BreadcrumbLink href="#" color={mainText}>
-            Add Inventory
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-      </Breadcrumb>
+      <Breadcrumb
+        firstLink={"Add Wapda Submeter"}
+        secondLink={"Add Inventory"}
+        heading={"Add Wapda Submeter"}
+        path={"/dashboard/add-inventory"}
+      />
       <CustomBox heading={"Add Wapda Submeter"} maxWidth={800}>
         <Formik initialValues={initialValues}>
           {({ errors, touched, values, handleBlur, handleChange }) => (
