93706448911874304d397bd668b4d381f43f543d
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-01-02 14:32:42 +0500

    Refactor useOwner hooks and add new functionality

--- src/hooks/useOwner.js
+++ src/hooks/useOwner.js
@@ -5,10 +5,25 @@ const owner = (values) => {
   return axios.post("/owner/add", values);
 };
 
-const showOwners = () => {
-  return axios.get("/owner/all");
+const showOwners = ({ queryKey }) => {
+  const search = queryKey[1];
+  const limit = queryKey[2];
+  const page = queryKey[3];
+  return axios.get(`/owner/all?search=${search}&limit=${limit}&page=${page}`);
 };
 
+const showOwner = ({ queryKey }) => {
+  const id = queryKey[1];
+  return axios.get(`/owner/${id}`);
+};
+
+const updateOwner = (id) => (values) => {
+  return axios.patch(`/owner/${id}/update`, values);
+};
+
+const updateImage = (id) => (values) => {
+  return axios.patch(`/owner/${id}/update-images`, values);
+};
 export const useAddOwner = (onSuccess, onError) => {
   return useMutation(owner, {
     onSuccess,
@@ -16,6 +31,24 @@ export const useAddOwner = (onSuccess, onError) => {
   });
 };
 
-export const useShowOwners = () => {
-  return useQuery("show-owners", showOwners);
+export const useShowOwners = (search = "", limit = 5, page = 1) => {
+  return useQuery(["show-owners", search, limit, page], showOwners);
+};
+
+export const useShowOwner = (id) => {
+  return useQuery(["show-owner", id], showOwner);
+};
+
+export const useUpdateOwner = (onSuccess, onError, id) => {
+  return useMutation(updateOwner(id), {
+    onSuccess,
+    onError,
+  });
+};
+
+export const useUpdateImage = (onSuccess, onError, id) => {
+  return useMutation(updateImage(id), {
+    onSuccess,
+    onError,
+  });
 };
