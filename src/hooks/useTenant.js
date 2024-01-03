7f7a4f44c42ba5ddad6b27502d0d561bf8d08c43
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-01-03 11:08:18 +0500

    Refactor useTenant hooks and add new functionalities

--- src/hooks/useTenant.js
+++ src/hooks/useTenant.js
@@ -5,8 +5,24 @@ const tenant = (values) => {
   return axios.post("/tenant/add", values);
 };
 
-const showTenants = () => {
-  return axios.get("/tenant/all");
+const showTenants = ({queryKey}) => {
+  const search = queryKey[1];
+  const limit = queryKey[2];
+  const page = queryKey[3];
+  return axios.get(`/tenant/all?search=${search}&limit=${limit}&page=${page}`);
+};
+
+const showTenant = ({ queryKey }) => {
+  const id = queryKey[1];
+  return axios.get(`/tenant/${id}`);
+};
+
+const updateTenant = (id) => (values) => {
+  return axios.patch(`/tenant/${id}/update`, values);
+};
+
+const updateImage = (id) => (values) => {
+  return axios.patch(`/tenant/${id}/update-images`, values);
 };
 
 export const useAddTenant = (onSuccess, onError) => {
@@ -16,6 +32,24 @@ export const useAddTenant = (onSuccess, onError) => {
   });
 };
 
-export const useShowTenants = () => {
-  return useQuery("show-tenants", showTenants);
+export const useShowTenants = (search="", limit=5, page=1) => {
+  return useQuery(["show-tenants", search, limit, page], showTenants);
+};
+
+export const useShowTenant = (id) => {
+  return useQuery(["show-tenant", id], showTenant);
+};
+
+export const useUpdateTenant = (onSuccess, onError, id) => {
+  return useMutation(updateTenant(id), {
+    onSuccess,
+    onError,
+  });
+};
+
+export const useUpdateImage = (onSuccess, onError, id) => {
+  return useMutation(updateImage(id), {
+    onSuccess,
+    onError,
+  });
 };
