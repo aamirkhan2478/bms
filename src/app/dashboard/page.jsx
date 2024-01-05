7f8826ca5662267ab975a273922969bff93ac4c1
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-01-05 11:20:16 +0500

    Refactor dashboard page component and add new features

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,10 +2,6 @@
 import Layout from "@/components/Layout";
 import SmallCard from "@/components/SmallCard";
 import {
-  Heading,
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
   useColorModeValue,
   SimpleGrid,
   Flex,
@@ -13,55 +9,122 @@ import {
   Box,
 } from "@chakra-ui/react";
 import {
+  MdApartment,
+  MdAssignment,
+  MdAssignmentAdd,
+  MdAssignmentLate,
   MdCalendarMonth,
-  MdCalendarToday,
   MdChecklist,
   MdPayments,
   MdReceipt,
-  MdWallet,
   MdWarning,
 } from "react-icons/md";
 import { useEffect } from "react";
+import { BiIdCard } from "react-icons/bi";
+import { IoMdCash } from "react-icons/io";
+import Breadcrumb from "@/components/Breadcrumb";
+import {
+  useInventoryOpenForSell,
+  useVacantInventories,
+} from "@/hooks/useInventory";
+import { useOwnerExpiredCnic } from "@/hooks/useOwner";
+import { useTenantExpiredCnic } from "@/hooks/useTenant";
+import { useExpireContracts } from "@/hooks/useContract";
 
 const Dashboard = () => {
-  let mainText = useColorModeValue("gray.700", "gray.200");
-  let secondaryText = useColorModeValue("gray.400", "gray.200");
+  const { data: openForSell, isLoading: openForSellLoading } =
+    useInventoryOpenForSell();
+  const { data: vacatedInventories, isLoading: vacatedInventoriesLoading } =
+    useVacantInventories();
+  const { data: ownerExpiredCnic, isLoading: ownerExpiredCnicLoading } =
+    useOwnerExpiredCnic();
+  const { data: tenantExpiredCnic, isLoading: tenantExpiredCnicLoading } =
+    useTenantExpiredCnic();
+  const { data: expireContracts, isLoading: expireContractsLoading } =
+    useExpireContracts();
 
   function currencyFormat(num) {
     return num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
   }
 
+  const totalOpenForSellInventoriesCount =
+    openForSell?.data?.data?.totalInventories;
+  const totalOfficeVacantInventoriesCount =
+    vacatedInventories?.data?.data?.vacantOfficeInventoriesCount;
+  const totalShopVacantInventoriesCount =
+    vacatedInventories?.data?.data?.vacantShopInventoriesCount;
+  const totalFlatVacantInventoriesCount =
+    vacatedInventories?.data?.data?.vacantFlatInventoriesCount;
+  const ownerExpiredCnicCount = ownerExpiredCnic?.data?.data?.expiredCnicCount;
+  const tenantExpiredCnicCount =
+    tenantExpiredCnic?.data?.data?.expiredCnicCount;
+  const expiringContractsCount =
+    expireContracts?.data?.data?.expiringContractsCount;
+  const expiredContractsCount =
+    expireContracts?.data?.data?.expiredContractsCount;
+
   const items = [
     {
       title: "Contract Expiring",
-      amount: 8,
+      amount: expiringContractsCount,
       icon: <MdCalendarMonth color="white" size={25} />,
+      isLoading: expireContractsLoading,
     },
     {
-      title: "Canceled Contract",
-      amount: 8,
-      icon: <MdCalendarToday color="white" size={25} />,
+      title: "Expired Contract",
+      amount: expiredContractsCount,
+      icon: <MdAssignmentLate color="white" size={25} />,
+      isLoading: expireContractsLoading,
     },
+    // {
+    //   title: "Contract to be renewed",
+    //   amount: 1,
+    //   icon: <MdAssignment color="white" size={25} />,
+    // },
     {
-      title: "Flat Open for sell",
-      amount: 8,
+      title: "Inventory Open for sell",
+      amount: totalOpenForSellInventoriesCount,
       icon: <MdChecklist color="white" size={25} />,
+      isLoading: openForSellLoading,
     },
     {
-      title: "Pending Invoices",
-      amount: currencyFormat(215000),
-      icon: <MdReceipt color="white" size={25} />,
+      title: "Vacant Flats | Shops | Offices",
+      amount: `${totalFlatVacantInventoriesCount} | ${totalShopVacantInventoriesCount} | ${totalOfficeVacantInventoriesCount}`,
+      icon: <MdApartment color="white" size={25} />,
+      isLoading: vacatedInventoriesLoading,
+    },
+    // {
+    //   title: "Pending Invoices",
+    //   amount: currencyFormat(215000),
+    //   icon: <MdReceipt color="white" size={25} />,
+    // },
+    // {
+    //   title: "Pending Payouts",
+    //   amount: currencyFormat(215000),
+    //   icon: <MdPayments color="white" size={25} />,
+    // },
+    {
+      title: "Owner CNIC Expired",
+      amount: ownerExpiredCnicCount,
+      icon: <BiIdCard color="white" size={25} />,
+      isLoading: ownerExpiredCnicLoading,
     },
     {
-      title: "Pending Payouts",
-      amount: currencyFormat(215000),
-      icon: <MdPayments color="white" size={25} />,
+      title: "Tenant CNIC Expired",
+      amount: tenantExpiredCnicCount,
+      icon: <BiIdCard color="white" size={25} />,
+      isLoading: tenantExpiredCnicLoading,
     },
-    {
-      title: "Agent Balance",
-      amount: currencyFormat(215000),
-      icon: <MdWallet color="white" size={25} />,
-    },
+    // {
+    //   title: "Contract Copy To Uploaded",
+    //   amount: 1,
+    //   icon: <MdAssignmentAdd color="white" size={25} />,
+    // },
+    // {
+    //   title: "Over Due pending Invoices",
+    //   amount: 1,
+    //   icon: <IoMdCash color="white" size={25} />,
+    // },
   ];
 
   useEffect(() => {
@@ -70,24 +133,12 @@ const Dashboard = () => {
 
   return (
     <Layout>
-      <Heading>Dashboard</Heading>
-      <Breadcrumb>
-        <BreadcrumbItem color={mainText}>
-          <BreadcrumbLink
-            href="#"
-            color={secondaryText}
-            textDecoration={"none"}
-          >
-            Dashboard
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-
-        <BreadcrumbItem color={mainText}>
-          <BreadcrumbLink href="#" color={mainText}>
-            Add Inventory
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-      </Breadcrumb>
+      <Breadcrumb
+        firstLink={"Dashboard"}
+        secondLink={"Add Inventory"}
+        heading={"Dashboard"}
+        path={"/dashboard/add-inventory"}
+      />
       <Flex
         bg={useColorModeValue("white", "#2D3748")}
         alignItems={"center"}
@@ -104,14 +155,14 @@ const Dashboard = () => {
         </Box>
       </Flex>
       <SimpleGrid columns={{ sm: 1, md: 2, xl: 3 }} spacing="15px" mt={"20px"}>
-        {items.map((item, index) => (
-          <Flex key={index} justifyContent={"center"}>
-            <SmallCard
-              title={item.title}
-              amount={item.amount}
-              icon={item.icon}
-            />
-          </Flex>
+        {items.map(({ title, amount, icon, isLoading }, index) => (
+          <SmallCard
+            key={index}
+            title={title}
+            amount={amount}
+            icon={icon}
+            isLoading={isLoading}
+          />
         ))}
       </SimpleGrid>
     </Layout>
