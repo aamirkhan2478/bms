d67309e730389fef91a47572b27f2dc398ccc186
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-01-05 11:20:55 +0500

    Add Skeleton loading effect to SmallCard component

--- src/components/SmallCard/index.jsx
+++ src/components/SmallCard/index.jsx
@@ -2,6 +2,8 @@
 // Chakra imports
 import {
   Flex,
+  Skeleton,
+  SkeletonText,
   Stat,
   StatLabel,
   StatNumber,
@@ -12,44 +14,59 @@ import {
 import Card from "../Card";
 import CardBody from "../Card/CardBody";
 
-const SmallCard = ({ title, amount, icon }) => {
+const SmallCard = ({ title, amount, icon, isLoading }) => {
   const iconTeal = useColorModeValue("teal.300", "teal.300");
   const textColor = useColorModeValue("gray.700", "white");
 
   return (
-    <Card minH="83px">
-      <CardBody>
-        <Flex flexDirection="row" align="center" justify="center" w="100%">
-          <Stat me="auto">
-            <StatLabel
-              fontSize="sm"
-              color="gray.400"
-              fontWeight="bold"
-              pb=".1rem"
-              textTransform={'uppercase'}
+    <Flex justifyContent={"center"}>
+      <Card minH="83px">
+        <CardBody>
+          <Flex flexDirection="row" align="center" justify="center" w="100%">
+            <Stat me="auto">
+              <SkeletonText
+                as={StatLabel}
+                fontSize="sm"
+                color="gray.400"
+                fontWeight="bold"
+                pb=".1rem"
+                textTransform={"uppercase"}
+                noOfLines={1}
+                w={isLoading ? "160px" : ""}
+                isLoaded={!isLoading}
+              >
+                {isLoading ? "Heading" : title}
+              </SkeletonText>
+              <Flex>
+                <SkeletonText
+                  as={StatNumber}
+                  fontSize="lg"
+                  color={textColor}
+                  mt={isLoading ? "0.5rem" : ""}
+                  h={isLoading ? "12px" : ""}
+                  noOfLines={1}
+                  isLoaded={!isLoading}
+                >
+                  {isLoading ? 22 : amount}
+                </SkeletonText>
+              </Flex>
+            </Stat>
+            <Skeleton
+              h={"45px"}
+              w={"45px"}
+              bg={iconTeal}
+              display={"flex"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              borderRadius={"12px"}
+              isLoaded={!isLoading}
             >
-              {title}
-            </StatLabel>
-            <Flex>
-              <StatNumber fontSize="lg" color={textColor}>
-                {amount}
-              </StatNumber>
-            </Flex>
-          </Stat>
-          <Flex
-            as="box"
-            h={"45px"}
-            w={"45px"}
-            bg={iconTeal}
-            alignItems={"center"}
-            justifyContent={"center"}
-            borderRadius={"12px"}
-          >
-            {icon}
+              {icon}
+            </Skeleton>
           </Flex>
-        </Flex>
-      </CardBody>
-    </Card>
+        </CardBody>
+      </Card>
+    </Flex>
   );
 };
 
