86c6397ab57efd4eff35e965458ec6c6bce3c916
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-01-05 11:21:02 +0500

    Add useExpireContracts hook to useContract.js

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -1,8 +1,12 @@
 import axios from "@/utils/axiosInstance";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 const addContract = (values) => {
   return axios.post("/contract/add", values);
+};
+
+const expireContracts = () => {
+  return axios.get("/contract/expire-contract");
 };
 
 export const useAddContract = (onSuccess, onError) => {
@@ -10,4 +14,8 @@ export const useAddContract = (onSuccess, onError) => {
     onSuccess,
     onError,
   });
+};
+
+export const useExpireContracts = () => {
+  return useQuery("expire-contracts", expireContracts);
 };
